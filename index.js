2897fbeb40565aa48745d00ebc4e13d190bf25e6
AlexBinkovskyy <alex_bin@ukr.net>
2024-04-09 15:53:24 +0300

    start

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const screen = document.querySelector(".screen p");
-const screenText = document.querySelector("p");
+// const screen = document.querySelector(".screen p");
+// const screenText = document.querySelector("p");
 document.querySelector(".ac").onclick = clearAll;
 
 const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
@@ -20,7 +20,6 @@ function clearAll() {
 }
 
 
-
 document.querySelector(".buttons").onclick = (event) => {
   if (
     !event.target.classList.contains("btn") ||
@@ -29,77 +28,8 @@ document.querySelector(".buttons").onclick = (event) => {
     return;
 
   const key = event.target.textContent;
+  console.log(key);
 
 
-  if(Number(key) === 0 && firstNum !== '') {
-    firstNum += key;
-    screen.textContent = firstNum;
-    return
-  }
 
-  if (
-    firstNum &&
-    secondNum &&
-    finish &&
-    !event.target.classList.contains("equal") &&
-    !actions.includes(key)
-  ) {
-    secondNum = "";
-    finish = false;
-  }
-
-  if (digit.includes(key) && !sign && !finish) {
-    firstNum += key;
-    if(firstNum[0] === '0') {
-      const temp = firstNum.slice(1)
-      firstNum = temp
-    }
-    screen.textContent = +firstNum
-    console.log(firstNum, sign, secondNum);
-  }
-
-  if (actions.includes(key) && firstNum) {
-    sign = key;
-    sign === "x"
-      ? (screen.textContent = `${+firstNum} *`)
-      : (screen.textContent = `${+firstNum} ${sign}`);
-    console.log(firstNum, sign, secondNum);
-    return;
-  }
-
-  if (sign && !event.target.classList.contains("equal")) {
-    finish = false;
-    secondNum += key;
-    sign === "x"
-      ? (screen.textContent = `${firstNum} * ${secondNum}`)
-      : (screen.textContent = `${firstNum} ${sign} ${secondNum}`);
-    console.log(firstNum, sign, secondNum);
-  }
-
-  if (event.target.classList.contains("equal") && firstNum) {
-    if (secondNum === "0" || Number(secondNum) === 0 && sign === '/') {
-      clearAll();
-      return (screen.textContent = "Ділення на 0!!!");
-    }
-    if (!secondNum) {
-      secondNum = +firstNum;
-    }
-    switch (sign) {
-      case "+":
-        firstNum = Number(+firstNum + +secondNum);
-        break;
-      case "-":
-        firstNum = firstNum - secondNum;
-        break;
-      case "x":
-        firstNum = firstNum * secondNum;
-        break;
-      case "/":
-        firstNum = firstNum / secondNum;
-        break;
-    }
-    screen.textContent = Number(firstNum).toFixed(2);
-    finish = true;
-    console.log(firstNum, sign, secondNum, finish);
-  }
 };
