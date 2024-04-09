2591b2574146b0693d77017ef523ffc8194d30b2
AlexBinkovskyy <alex_bin@ukr.net>
2024-04-09 13:43:25 +0300

    ііі

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
-const screen = document.querySelector('.screen p');
-const screenText = document.querySelector('p')
+const screen = document.querySelector(".screen p");
+const screenText = document.querySelector("p");
 document.querySelector(".ac").onclick = clearAll;
 
 const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
 const actions = ["-", "+", "x", "/"];
+const percent = false;
+const plusMinus = false;
 let firstNum = "";
 let secondNum = "";
-let totall = "";
 let sign = "";
 let finish = false;
 
 function clearAll() {
   firstNum = "";
   secondNum = "";
-  totall = "";
   sign = "";
   finish = false;
   screen.textContent = 0;
 }
 
-function resizeFont () {
-  if (screenText.textContent.length >= 7) {
-    screenText.style.fontSize= "2rem";
-    screenText.style.height= "auto";
-    screenText.style.width= "100%";
-    screenText.style.alignContent= "end";
-    screenText.style.margin= "0 0 20px 0";
-    // screenText.style.textAlign= "end";
-    // screenText.style.textWrap= "wrap";
-
-  }
-}
-
 document.querySelector(".buttons").onclick = (event) => {
-  resizeFont ()
+
   if (
     !event.target.classList.contains("btn") ||
     event.target.classList.contains("ac")
@@ -42,42 +29,65 @@ document.querySelector(".buttons").onclick = (event) => {
 
   const key = event.target.textContent;
 
-  if (digit.includes(key) && !sign) {
-    firstNum += key;
+  if (
+    firstNum &&
+    secondNum &&
+    !finish &&
+    !event.target.classList.contains("equal") &&
+    !actions.includes(key)
+  ) {
+    clearAll();
+  }
+
+  if (digit.includes(key) && !sign && !finish) {
+    if(firstNum) return secondNum = +key;
+    
+    firstNum += +key;
     screen.textContent = firstNum;
     console.log(firstNum, sign, secondNum);
   }
 
   if (actions.includes(key) && firstNum) {
     sign = key;
-    sign === 'x' ? (screen.textContent = "*") : (screen.textContent = key);
+    sign === "x"
+      ? (screen.textContent = "*")
+      : (screen.textContent = firstNum, `${sign}`);
     console.log(firstNum, sign, secondNum);
     return;
   }
 
   if (sign && !event.target.classList.contains("equal")) {
-    secondNum += key;
-    screen.textContent = secondNum;
+    if(secondNum && finish) secondNum = 0
+    finish = false;
+    secondNum += +key;
+    screen.textContent = `${firstNum} ${sign} ${secondNum}`;
     console.log(firstNum, sign, secondNum);
   }
 
   if (event.target.classList.contains("equal") && firstNum) {
+    if (secondNum === "0" || Number(secondNum) === 0) {
+      clearAll();
+      return (screen.textContent = "Ділення на 0!!!");
+    }
+    if (!secondNum) {
+      secondNum = +firstNum;
+    }
     switch (sign) {
       case "+":
-        totall = +firstNum + +secondNum;
+        firstNum = Number(+firstNum + +secondNum);
         break;
       case "-":
-        totall = firstNum - secondNum;
+        firstNum = +firstNum - +secondNum;
         break;
       case "x":
-        totall = firstNum * secondNum;
+        firstNum = +firstNum * +secondNum;
         break;
       case "/":
-        totall = firstNum / secondNum;
+        firstNum = +firstNum / +secondNum;
         break;
     }
-    screen.textContent = (+totall)
-    finish = !finish;
-    console.log(firstNum, sign, secondNum, totall);
+    screen.textContent = +firstNum;
+    finish = true;
+    console.log(firstNum, sign, +secondNum, finish);
   }
 };
